29d690d0f3e9e9f48209de47ffde85a8a8f06631
0027025249 <yao.zepeng@iwhalecloud.com>
2022-08-25 15:51:01 +0800

    project and modules initialization

---
+++ ponder-quota-todo-starter/src/main/java/io/github/pquota/todo/PonderQuotaTodoApplication.java
@@ -0,0 +1,13 @@
+package io.github.pquota.todo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PonderQuotaTodoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PonderQuotaTodoApplication.class, args);
+    }
+
+}
