3db0577b94b76d6c2fa5d7aa2869690a0d605686
0027025249 <yao.zepeng@iwhalecloud.com>
2022-09-26 19:59:45 +0800

    config maven dependencies

--- ponder-quota-todo-starter/src/main/java/io/github/pquota/todo/PonderQuotaTodoApplication.java
+++ ponder-quota-todo-starter/src/main/java/io/github/pquota/todo/PonderQuotaTodoApplication.java
@@ -2,8 +2,12 @@ package io.github.pquota.todo;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.transaction.annotation.EnableTransactionManagement;
 
 @SpringBootApplication
+@EnableTransactionManagement
+@ComponentScan(basePackages = {"io.github.pquota", "io.github.ponderyao.ddd"})
 public class PonderQuotaTodoApplication {
 
     public static void main(String[] args) {
